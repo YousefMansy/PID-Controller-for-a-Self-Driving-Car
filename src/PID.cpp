fde2667a8b6006b3341103c17453bb8798b4c61c
YousefMansy <y.o.mansy@gmail.com>
2019-02-13 00:09:45 +0200

    Final Submission

--- src/PID.cpp
+++ src/PID.cpp
@@ -4,27 +4,35 @@
  * TODO: Complete the PID class. You may add any additional desired functions.
  */
 
-PID::PID() {}
+PID::PID(double Kp_, double Ki_, double Kd_)
+{ /**
+   * Initialize PID coefficients and errors
+   */
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+
+  p_error = 0;
+  i_error = 0;
+  d_error = 0;
+}
 
 PID::~PID() {}
 
-void PID::Init(double Kp_, double Ki_, double Kd_) {
+void PID::UpdateError(double cte)
+{
   /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
+   * Update PID errors based on cte.
    */
-
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
 }
 
-void PID::UpdateError(double cte) {
+double PID::TotalError()
+{
   /**
-   * TODO: Update PID errors based on cte.
+   * Calculate and return the total error
    */
-
-}
-
-double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+  return -Kp * p_error - Kd * d_error - Ki * i_error;
 }
--- src/PID.h
+++ src/PID.h
@@ -1,23 +1,19 @@
 #ifndef PID_H
 #define PID_H
 
-class PID {
- public:
+class PID
+{
+public:
   /**
    * Constructor
+   * @param (Kp_, Ki_, Kd_) The initial PID coefficients
    */
-  PID();
+  PID(double Kp_, double Ki_, double Kd_);
 
   /**
    * Destructor.
    */
   virtual ~PID();
-
-  /**
-   * Initialize PID.
-   * @param (Kp_, Ki_, Kd_) The initial PID coefficients
-   */
-  void Init(double Kp_, double Ki_, double Kd_);
 
   /**
    * Update the PID error variables given cross track error.
@@ -31,7 +27,7 @@ class PID {
    */
   double TotalError();
 
- private:
+private:
   /**
    * PID Errors
    */
@@ -41,10 +37,10 @@ class PID {
 
   /**
    * PID Coefficients
-   */ 
+   */
   double Kp;
   double Ki;
   double Kd;
 };
 
-#endif  // PID_H
+#endif // PID_H
